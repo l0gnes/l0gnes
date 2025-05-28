7e749c15f852484ed5fa9fc6371a9bc85a02a3e3
Alex <97571297+l0gnes@users.noreply.github.com>
2025-05-28 02:19:07 -0300

    > exclude YAML as a language

--- generate.py
+++ generate.py
@@ -5,6 +5,9 @@ from utils import ascii_progress_bar
 
 STATS_URL = "https://wakatime.com/api/v1/users/lognes/stats"
 
+# Most of YAML is Kubernetes, but I don't really care for it to be shown on my profile.
+EXCLUDED_LANGS = ["YAML"]
+
 # Open up the jinja template readme
 with open('./template-readme.md', 'r', encoding='utf-8') as readme_in:
     template = jinja2.Template(source = readme_in.read())
@@ -12,7 +15,10 @@ with open('./template-readme.md', 'r', encoding='utf-8') as readme_in:
 req = requests.get(STATS_URL)
 resp = req.json()
 
-top_langs = resp['data']['languages']
+top_langs = filter(
+    lambda n: n["name"] not in EXCLUDED_LANGS,
+    resp['data']['languages']
+)
 
 # Export compiled readme
 with open("./readme.md", "w+", encoding="utf-8") as readme_out:
