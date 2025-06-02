70399386cbdb2b6b3449c1420df8ad388cdcf663
l0gnes <loonargt@gmail.com>
2025-06-02 18:13:26 -0300

    > scuffed rust rewrite just for fun

--- utils.py
+++
@@ -1,9 +0,0 @@
-def ascii_progress_bar(
-    progress : float, # between 0 - 1
-    char_length : int = 24,
-    empty_char : str = ".",
-    filled_char : str = "█"
-) -> str:
-    filled_squares = int(char_length * progress)
-    empty_squares = char_length - filled_squares
-    return '[' + (filled_char * filled_squares) + (empty_char * empty_squares) + ']'
